63e21122f8df7f35482a61d4984a35f4c6b00c40
Nabie6 <samiraazarkulova19@mail.ru>
2026-01-26 13:41:01 +0600

    homework

---
+++ hello.py
@@ -0,0 +1,6 @@
+name=str(input("What is your name?   "))
+age=int(input("how old are you?   "))
+if age >= 18: 
+    print(f"hello, {name} you are an adult")
+else:
+    print(f" hello, {name} you are a minnor")
---
+++ loop.py
@@ -0,0 +1,20 @@
+
+"""for i in range(1, 56):
+ #print (i)
+if i % == 0:
+    print 
+   
+  
+a = 1
+while a <= 55:
+    print(a)
+  
+def odd (a):
+    return a*3
+
+if a % 2 == 0:
+ result = odd(a)
+else:
+   print ("Even")
+     
+  a +=1"""
