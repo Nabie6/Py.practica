ea6773f24cf299f2b038ebf0924070c75d2abf47
Nabie6 <samiraazarkulova19@mail.ru>
2026-02-12 15:20:36 +0600

    Add files via upload

---
+++ slices.py
@@ -0,0 +1,33 @@
+
+s="PythonPrograming"
+print(s[:6])
+
+word="Hello"
+print(word[::-1])
+
+phrase = "abcdefghijklmno"
+print(phrase[1::2])
+
+sentence = "DataScience"
+print(sentence[-4:])
+
+new = "MachineLearning"
+print(new[7:12])
+
+string = "abcdefg"
+print(string[0::6])
+
+a= "Python"
+print(a[1::1])
+
+code = "programming"
+print(code[1::2])
+
+email = "student@university.com"
+print(email[8:18])
+
+palindrome = "madam"
+if palindrome == palindrome[::-1]:
+    print("word palindrome")
+else:
+    print("not palindrome")
